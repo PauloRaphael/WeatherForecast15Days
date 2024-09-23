46c806b923fe992630144b59f5a86b0979a0e2f4
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-09-23 10:12:41 -0300

    Adding the sun

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const URI_270_DAYS = `http://apiadvisor.climatempo.com.br/api/v1/forecast/locale
 
 const HAPPINESS_THRESHOLD = 18;
 const BEARABLE_THRESHOLD = 25;
+const UNBERABLE_TRESHOLD = 32;
 
 const THERMOMETER_GAIN_ICON =  "<span class=\"material-symbols-outlined\">thermometer_gain</span>";
 const THERMOMETER_MINUS_ICON = "<span class=\"material-symbols-outlined\">thermometer_minus</span>";
 const CALENDAR_ICON = "<span class=\"material-symbols-outlined\">calendar_month</span>"
 
 const IMAGES = {
+    death: "https://hips.hearstapps.com/pop.h-cdn.co/assets/17/24/1024x1024/square-1497634598-eithe-981211-big.gif?resize=980:*",
     hot: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcqX1y4reo4wVEMfu53871t0Sv7v-vb9A31Q&s",
     warm: "https://cdn-icons-png.flaticon.com/512/158/158420.png",
     cold: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQilahueN_sF9N0AOquT4rFL1o_qcsttwB3Gg&s"
@@ -45,7 +47,9 @@ fetch(URI_270_DAYS)
     });
 
 const getWeatherImage = (temperature) => {
-    if (temperature > BEARABLE_THRESHOLD) {
+    if(temperature > UNBERABLE_TRESHOLD) {
+        return IMAGES.death
+    } else if (temperature > BEARABLE_THRESHOLD) {
         return IMAGES.hot;
     } else if (temperature > HAPPINESS_THRESHOLD) {
         return IMAGES.warm;
